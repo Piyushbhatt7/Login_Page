3f484a62a1822d2fd3fe0db7fe7013a0836c0686
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-10 22:23:56 +0530

    signin screen

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -68,7 +68,7 @@ class _SignInState extends State<SignIn> {
               
                  TextField(
                    decoration: InputDecoration(
-
+                    hintText: 'Enter Email'
                    ),
                  )
               
