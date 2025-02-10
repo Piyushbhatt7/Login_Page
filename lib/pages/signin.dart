1abc5c1ea2feeff8a7fa42e5e2de25cb29cca730
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-10 21:59:17 +0530

    signin screen

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -13,6 +13,20 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Container(
+
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient:
+          LinearGradient(colors: [
+            Color(0xffB51837),
+            Color(0xff661C3A),
+            Color(0xff301939)
+          ],
+              begin: Alignment.topLeft, end: Alignment.topRight
+
+          ),
+        ),
         child: Column(
           children: [
 
