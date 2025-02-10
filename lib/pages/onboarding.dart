6753232a26ff5c4945fa374f0b3aa24f2cdb946e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-10 21:50:38 +0530

    onboarding screen

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -96,7 +96,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   fontSize: 20,),
             ),
 
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
