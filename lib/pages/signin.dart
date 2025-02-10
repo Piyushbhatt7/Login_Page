65965f65bed134c266114255713e432ff63542fc
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-10 22:10:03 +0530

    signin screen

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -9,11 +9,12 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 50.0, left: 30.0),
+        padding: EdgeInsets.only(top: 50.0),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
@@ -31,14 +32,18 @@ class _SignInState extends State<SignIn> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
 
-            Text(
-              "Hello\nSign in!",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Text(
+                "Hello\nSign in!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
+            SizedBox(height: 40.0,),
 
             Container(
               width: MediaQuery.of(context).size.width,
@@ -48,6 +53,8 @@ class _SignInState extends State<SignIn> {
               ),
               child: Column(
                 children: <Widget>[
+
+
 
                 ],
               ),
