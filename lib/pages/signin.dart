97819d30159ef1a67b976ed818db2c746e03a81e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-10 23:07:51 +0530

    signin screen

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -43,7 +43,7 @@ class _SignInState extends State<SignIn> {
                 ),
               ),
             ),
-            SizedBox(height: 35.0,),
+            SizedBox(height: 25.0,),
 
             Expanded(
               child: Container(
